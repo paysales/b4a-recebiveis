b64e19548a9d306eba7c59ec32dd8c20b6dd8d91
Richard Cotrim <richardcotrim@gmail.com>
2025-04-18 18:05:05 -0300

    jwt B3 !

--- cloud/features/webhook.js
+++ cloud/features/webhook.js
@@ -2,7 +2,6 @@
 const jwt = require('jsonwebtoken');
 const oauth = Parse.Object.extend('OAuth');
 
-
 function generateToken(payload, options) {
     const jwtSecret = process.env.JWT_SECRET; // Use a mesma chave secreta
 
@@ -21,14 +20,14 @@ function validateToken(token) {
     if (!token) {
         return { isValid: false, error: "Token não fornecido." };
     }
-    console.log("Token a ser validado:", token);
+
     try {
         const decoded = jwt.verify(token, jwtSecret);
         // Se a verificação for bem-sucedida, 'decoded' conterá o payload do token
-        console.log("Token decodificado:", decoded);
+        // console.log("Token decodificado:", decoded);
         return { isValid: true, payload: decoded };
     } catch (error) {
-        console.error("Erro ao validar JWT:", error);
+        // console.error("Erro ao validar JWT:", error);
         return { isValid: false, error: error.message };
     }
 }
@@ -36,7 +35,7 @@ function validateToken(token) {
 
 
 
-Parse.Cloud.define('webhook', async (req) => {
+Parse.Cloud.define('webhook-teste', async (req) => {
     if (req.user == null) throw 'USER_NOT_FOUND';
     if (req.user.id != 'kdETA7qa25') throw 'USER_NOT_AUTHORIZED';
     return 'Olá desde a back4app da AppSales.....'
@@ -45,6 +44,9 @@ Parse.Cloud.define('webhook', async (req) => {
 });
 
 Parse.Cloud.define('webhook-auth', async (req) => {
+
+    if (req.user == null) throw 'USER_NOT_FOUND';
+    if (req.user.id != 'kdETA7qa25') throw 'USER_NOT_AUTHORIZED';
 
     const { client_id, client_secret } = req.params;
 
@@ -83,13 +85,14 @@ Parse.Cloud.define('webhook-auth', async (req) => {
         audience: source
     };
 
-    const accessToken = auth.generateToken(payload, options); // Implemente sua lógica de geração de token
+    const accessToken = generateToken(payload, options); // Implemente sua lógica de geração de token
     return {
         accessToken: accessToken,
         tokenType: "Bearer",
         expiresIn: expiresIn,
     };
 }, {
+    requireUser: true,
     fields: {
         client_id: {
             required: true
@@ -100,10 +103,10 @@ Parse.Cloud.define('webhook-auth', async (req) => {
     }
 });
 
-Parse.Cloud.define('webhook-validate', async (req) => {
+Parse.Cloud.define('webhook', async (req) => {
 
-    // if (req.user == null) throw 'USER_NOT_FOUND';
-    // if (req.user.id != 'kdETA7qa25') throw 'USER_NOT_AUTHORIZED';
+    if (req.user == null) throw 'USER_NOT_FOUND';
+    if (req.user.id != 'kdETA7qa25') throw 'USER_NOT_AUTHORIZED';
 
     const authorizationHeader = req.headers["authorization"];
 
@@ -117,7 +120,7 @@ Parse.Cloud.define('webhook-validate', async (req) => {
     }
     const accessToken = parts[1];
 
-    const validationResult = await auth.validateToken(accessToken);
+    const validationResult = validateToken(accessToken);
 
     if (!validationResult.isValid) {
         console.error("Token inválido:", validationResult.error);
@@ -125,10 +128,10 @@ Parse.Cloud.define('webhook-validate', async (req) => {
     }
 
     const b3Info = validationResult.payload;
-    console.log("Informações da B3 do token:", b3Info);
 
     // Agora você pode processar a requisição do webhook com a garantia de que ela (teoricamente) veio da B3
-
     return { success: true, message: "Webhook recebido e processado com sucesso.", info: b3Info };
+}, {
+    requireUser: true
 });
 
